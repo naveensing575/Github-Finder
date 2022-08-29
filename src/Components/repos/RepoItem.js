1e0a05062e2d2e9819f74059e95636a187001754
naveensing575 <naveensing575@gmail.com>
2022-08-29 14:11:21 +0530

    changed spinner

--- src/Components/repos/RepoItem.js
+++ src/Components/repos/RepoItem.js
@@ -5,7 +5,7 @@ const RepoItem = ({ repo }) => {
   return (
     <div className='card'>
       <h3>
-        <a href={repo.html_url}>{repo.name}</a>
+        <a className='url' href={repo.html_url}>{repo.name}</a>
       </h3>
     </div>
   );
